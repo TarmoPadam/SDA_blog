a25b581160a2e879903654a3dd5bd45ca1e49ded
Tarmo Padam <tarmo@hotmail.com>
2023-11-04 15:06:34 +0200

    Did the Authentication

---
+++ posts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import all_posts, create_post
+
+
+urlpatterns = [
+    path("", all_posts, name='home'),
+    path("posts/create/", create_post, name='create_post')
+]
