c13d2c5bdecf4f2b8e6badde0a8fb204b0ff1619
Tarmo Padam <tarmo@hotmail.com>
2023-11-18 10:54:14 +0200

    added Edit function

--- blog_root/settings.py
+++ blog_root/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'widget_tweaks',
     'posts',
+    'about',
 ]
 
 MIDDLEWARE = [
--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.all_posts, name='home'),
     path("posts/create/", views.create_post, name='create_post'),
-    path("posts/<int:pk>", views.post_detail, name='post_detail')
+    path("posts/<int:pk>", views.post_detail, name='post_detail'),
+    path("posts/<int:pk>/update", views.post_update, name='post_update'),
 ]
